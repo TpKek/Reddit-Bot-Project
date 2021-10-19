e471dff18a7be64cebaa4143e1c43aefd44724ce
TpK <Bertin.Dreyer1@gmail.com>
2021-10-19 16:40:44 +0200

    Rebranched due to personal info (Oops) and pushed final code of project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reddit Bot Project
+ My first mini-project
+ 
+This Reddit bot can read the titles of posts from any subreddit chosen by the user.
+It also looks for specific words in each comment on a subreddit post and replies with a comment of its own.
+
+Bot in action:
+
+![RBot](https://user-images.githubusercontent.com/92753848/137928905-d5ff291b-6680-4a26-8ba9-ed5da5e64851.PNG)
---
+++ bot.py
@@ -0,0 +1,34 @@
+import praw
+from praw.models import MoreComments
+
+#creating a reddit instance (if you want to read public posts, there is no need to login)
+reddit = praw.Reddit(
+    client_id="",
+    client_secret="",
+    user_agent="",
+    username="RBotProject",
+    password="")
+
+#grab a subreddit from reddit using reddit instance
+subreddit = reddit.subreddit("RBotProject")
+
+#loop through first 10 hot submissions in subreddit and post the title and score
+for submission in subreddit.hot(limit=10):
+    print("**********")
+    #print title of posts
+    print(submission.title)
+    #print post score(upvotes - downvotes)
+    print("Score: ", submission.score)
+
+    #loop through all comments in a submission and look for the word Google
+    for comment in submission.comments:
+        #making sure all the words in the comment is lowercase
+        comment_lower = comment.body.lower()
+        #This is to ignore the "Load more comments" object on Reddit
+        if isinstance(comment, MoreComments):
+            continue
+        if " test " in comment_lower:
+            print("--------")
+            print(comment.body)
+            #If the word "test" is in the comment, reply with:
+            comment.reply("It did! *Bot Reply*")
